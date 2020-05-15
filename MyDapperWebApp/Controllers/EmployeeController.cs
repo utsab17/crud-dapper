8ab8e8b20fbdd7575dd8e6b2ef242316956c5b30
Utsab Upadhyay <utsab17@gmail.com>
2020-05-15 23:44:36 +0000

    Completed app

---
+++ MyDapperWebApp/Controllers/EmployeeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using MyDapperWebApp.Models;
+
+namespace MyDapperWebApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(Models.DapperORM.ReturnList<EmployeeModel>("EmployeeViewAll" ));
+        }
+        [HttpGet]
+        public IActionResult AddOrEdit(int id = 0)
+        {
+
+            if (id == 0)
+                return View();
+            else {
+                DynamicParameters param = new DynamicParameters();
+                param.Add("@EmployeeID", id);
+                return View(DapperORM.ReturnList<EmployeeModel>("EmployeeViewByID", param).FirstOrDefault<EmployeeModel>());
+             }
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(EmployeeModel emp)
+        {
+            Dapper.DynamicParameters param = new Dapper.DynamicParameters();
+            param.Add("@EmployeeID", emp.EmployeeId);
+            param.Add("@Name", emp.Name);
+            param.Add("@Position", emp.Position);
+            param.Add("@Age", emp.Age);
+            param.Add("@Salary", emp.Salary);
+
+            DapperORM.ExecuteWithoutReturn("EmployeeAddOrEdit", param);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MyDapperWebApp/Models/DapperORM.cs
@@ -0,0 +1,56 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyDapperWebApp.Models
+{
+    public static class DapperORM
+    {
+        private static string connectionString = "Data Source=EC2AMAZ-EAKN5QJ;Database=DapperDB;Integrated Security=True";
+
+
+        public static void ExecuteWithoutReturn(string procedureName, DynamicParameters param = null) { 
+        
+           using(SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+
+                sqlCon.Open();
+                sqlCon.Execute(procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
+
+            }
+        
+        }
+
+        public static T ExecuteReturnScalar<T>(string procedureName, DynamicParameters param = null)
+        {
+
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+
+                sqlCon.Open();
+                return (T)Convert.ChangeType(
+                    sqlCon.ExecuteScalar(procedureName, param, commandType: System.Data.CommandType.StoredProcedure), typeof (T));
+
+            }
+
+        }
+
+
+        public static IEnumerable<T> ReturnList<T>(string procedureName, DynamicParameters param = null)
+        {
+
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+
+                sqlCon.Open();
+               return sqlCon.Query<T>(procedureName, param, commandType: System.Data.CommandType.StoredProcedure);
+
+            }
+
+        }
+
+    }
+}
